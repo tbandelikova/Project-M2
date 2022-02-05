e8ab3f138c61cfcc87542e024ca9f31dcabc666a
tat.bandelikova <tat.bandelikova@gmail.com>
2022-02-05 21:34:31 +0300

    Кнопка добавить

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-{\rtf1}
+let addButton = document.querySelector('.add');
+let removeB = '<button class="remove">X</button>';
+let taskList = document.querySelector('.task-list');
+
+function addTask() {
+    let newTask = document.createElement('div');
+    newTask.innerHTML = `<p>${document.querySelector('.task').value}</p>${removeB}`;
+    taskList.append(newTask);
+    newTask.style.display = 'flex';
+    taskList.style.display = 'block';
+
+}
+
+addButton.addEventListener('click', addTask);
