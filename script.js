9194a3b7a0c3115a074998242eac6348f7df02f3
tat.bandelikova <tat.bandelikova@gmail.com>
2022-02-19 10:43:43 +0300

    sort

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let taskList = document.querySelector('.new-task');
-let newTask = `<div class="item"><input class="task" type="text" maxlength="27">
-<button class="delete">X</button></div>`;
+let sortButton = document.querySelector('.sort-tasks');
+let sortList = document.querySelectorAll('.sortEl');
 
 document.querySelector('.action').addEventListener('click', () => {
-    taskList.innerHTML += newTask;
+    let newTask = document.createElement('div');
+    newTask.classList.add('item', 'sortEl');
+    newTask.innerHTML = `<input class="task" type="text" maxlength="27">
+    <button class="delete">X</button>`;
+    taskList.append(newTask);
 
     let deleteTask = document.querySelectorAll('.delete');
 
@@ -13,3 +17,30 @@ document.querySelector('.action').addEventListener('click', () => {
         } )
     })
 });
+
+sortButton.addEventListener('click', () => {
+    let sortRow = [];
+    document.querySelectorAll('.task').forEach((item) => {
+        sortRow.push(item.value);
+        sortRow.sort(compare);
+
+        function compare(a, b) {
+            if (!NaN) {
+                return a - b;
+            }
+          return (a.querySelector('.task').innerText).localeCompare(b.querySelector('.task').innerText);
+        };
+        // item.value  
+    });
+    console.log(sortRow);
+    
+})
+
+sortButton.addEventListener('mouseover', () => {
+    if (sortButton.innerHTML == '<img src="./img/Group 34.png" alt="alphabet"></img>') {
+        this.innerHTML = '<img src="./img/Group 73.png" alt="alphabet"></img>';
+        console.log("!!!");
+    } else {
+        this.innerHTML = '<img src="./img/Group 91.png" alt="alphabet"></img>';
+    }
+})
